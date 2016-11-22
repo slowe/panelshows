a7ccd7b3d94d85dd4c231b6ffe5fc2e001eaf0ce
Stuart <gravatar@strudel.org.uk>
2016-11-22 22:16:32 +0000

    Add new series of 8 Out of 10 Cats

---
+++ matrix2.js
@@ -0,0 +1,86 @@
+
+function ready(e){/in/.test(document.readyState)?setTimeout("ready("+e+")",9):e()};
+
+// Example of force-directed layout from Mbostock
+ready(function(){
+
+
+	var svg = d3.select("svg"),
+		width = +svg.attr("width"),
+		height = +svg.attr("height");
+
+	var color = d3.scaleOrdinal(d3.schemeCategory20);
+
+	var simulation = d3.forceSimulation()
+		.force("link", d3.forceLink().id(function(d) { return d.id; }))
+		.force("charge", d3.forceManyBody())
+		.force("center", d3.forceCenter(width / 2, height / 2));
+
+	d3.json("people2.json", function(error, graph) {
+		if (error) throw error;
+		for(var i = 0; i < graph.nodes.length; i++){
+			graph.nodes[i].id = i;
+		}
+
+		var link = svg.append("g")
+			.attr("class", "links")
+			.selectAll("line")
+			.data(graph.links)
+			.enter().append("line")
+			.attr("stroke-width", function(d) { return Math.sqrt(d.value); });
+
+		var node = svg.append("g")
+			.attr("class", "nodes")
+			.selectAll("circle")
+			.data(graph.nodes)
+			.enter().append("circle")
+			.attr("r", 3)
+			.attr("fill", function(d) { return d.group == 1 ? "#0dbc37" : "#722ea5"; })
+			.call(d3.drag()
+				.on("start", dragstarted)
+				.on("drag", dragged)
+				.on("end", dragended));
+
+		node.append("title")
+		  .text(function(d) { return d.name; });
+
+	  simulation
+		  .nodes(graph.nodes)
+		  .on("tick", ticked);
+
+	  simulation.force("link")
+		  .links(graph.links);
+
+	console.log(graph.nodes)
+
+
+	  function ticked() {
+		link
+			.attr("x1", function(d) { return d.source.x; })
+			.attr("y1", function(d) { return d.source.y; })
+			.attr("x2", function(d) { return d.target.x; })
+			.attr("y2", function(d) { return d.target.y; });
+
+		node
+			.attr("cx", function(d) { return d.x; })
+			.attr("cy", function(d) { return d.y; });
+	  }
+	});
+
+	function dragstarted(d) {
+	  if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+	  d.fx = d.x;
+	  d.fy = d.y;
+	}
+
+	function dragged(d) {
+	  d.fx = d3.event.x;
+	  d.fy = d3.event.y;
+	}
+
+	function dragended(d) {
+	  if (!d3.event.active) simulation.alphaTarget(0);
+	  d.fx = null;
+	  d.fy = null;
+	}
+});
